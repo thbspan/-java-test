06ac99bf34225fae768e98db7412be8695d0f279
thbspan <thbspan@163.com>
2020-06-11 22:09:34 +0800

    Objenesis/反序列化不调用构造函数实例化对象原理

---
+++ src/test/java/com/test/reflect/InstanceExample.java
@@ -0,0 +1,14 @@
+package com.test.reflect;
+
+public class InstanceExample {
+    private int i;
+
+    public InstanceExample(int i){
+        this.i = i;
+    }
+
+    public void show(){
+        System.out.println("instance example ..." + i);
+    }
+
+}
---
+++ src/test/java/com/test/reflect/ObjenesisTest.java
@@ -0,0 +1,36 @@
+package com.test.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import sun.reflect.ReflectionFactory;
+
+public class ObjenesisTest {
+
+    @Test
+    public void testInstanceClass() {
+        Objenesis objenesis = new ObjenesisStd(true);
+        InstanceExample example = objenesis.newInstance(InstanceExample.class);
+
+        example.show();
+    }
+
+    /**
+     * ObjenesisStd 底层实现
+     * <br/>
+     * 反序列化不调用构造函数实例化对象原理
+     */
+    @Test
+    public void testInstanceClassOrigin() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        ReflectionFactory reflectionFactory = ReflectionFactory.getReflectionFactory();
+        Constructor<Object> objectConstructor = Object.class.getConstructor((Class<?>[]) null);
+        Constructor<?> constructor = reflectionFactory.newConstructorForSerialization(InstanceExample.class, objectConstructor);
+        constructor.setAccessible(true);
+        InstanceExample example = (InstanceExample) constructor.newInstance();
+        example.show();
+    }
+}
