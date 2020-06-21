70efff1719677c95c87ac4cd371802ab73f90122
thbspan <thbspan@163.com>
2020-06-21 20:07:00 +0800

    Inheritable ThreadLocal test

---
+++ src/test/java/com/test/oom/RuntimeConstantPoolOOMTest.java
@@ -0,0 +1,22 @@
+package com.test.oom;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class RuntimeConstantPoolOOMTest {
+
+    /**
+     * <= JDK 6 -XX:PermSize=6M -XX:MaxPermSize=6M; -Xmx=6M
+     *
+     */
+    @Test
+    public void test() {
+        Set<String> set = new HashSet<>();
+        short i = 0;
+        while (true) {
+            set.add(String.valueOf(i++).intern());
+        }
+    }
+}
---
+++ src/test/java/com/test/thread/InheritableThreadLocalTest.java
@@ -0,0 +1,21 @@
+package com.test.thread;
+
+import org.junit.Test;
+
+public class InheritableThreadLocalTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        inheritableThreadLocal.set("test inheritable");
+
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        threadLocal.set("test thread local");
+        Thread thread = new Thread(() -> {
+            System.out.println(inheritableThreadLocal.get());
+            System.out.println(threadLocal.get());
+        });
+        thread.start();
+        thread.join();
+    }
+}
