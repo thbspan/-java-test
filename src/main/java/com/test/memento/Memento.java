216e15ff014a8618d311a9b342c887c9226ce2e1
thbspan <thbspan@163.com>
2020-10-29 09:28:54 +0800

    add memento pattern

---
+++ src/main/java/com/test/memento/Memento.java
@@ -0,0 +1,13 @@
+package com.test.memento;
+
+public class Memento {
+    private final String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
